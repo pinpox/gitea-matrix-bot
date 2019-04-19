a0e6f2943ab04966b1d0c5e96420a4a703588143
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2019-04-19 11:18:02 +0200

    comment out tests

--- listener_test.go
+++ listener_test.go
@@ -5,36 +5,36 @@ import (
 	"testing"
 )
 
-func Test_setupListener(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			setupListener()
-		})
-	}
-}
+// func Test_setupListener(t *testing.T) {
+// 	tests := []struct {
+// 		name string
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			setupListener()
+// 		})
+// 	}
+// }
 
-func TestPostHandler(t *testing.T) {
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			PostHandler(tt.args.w, tt.args.r)
-		})
-	}
-}
+// func TestPostHandler(t *testing.T) {
+// 	type args struct {
+// 		w http.ResponseWriter
+// 		r *http.Request
+// 	}
+// 	tests := []struct {
+// 		name string
+// 		args args
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			PostHandler(tt.args.w, tt.args.r)
+// 		})
+// 	}
+// }
 
 func Test_generateMessage(t *testing.T) {
 	type args struct {
